15ff13e73e1ba5716e518375ab077248cdcb4ee1
rteina <rteina@galigeo.com>
2020-10-15 09:59:48 +0200

    Update 01_setup_env.sh

--- 01_setup_env.sh
+++ 01_setup_env.sh
@@ -20,8 +20,9 @@ echo "Setting DATA_DIR variable to ./data directory to path in to .env file"
 sed -i '/DATA_DIR/d' .env
 echo 'DATA_DIR=./data' >> .env
 
+# [DEPRECATED]
 # configure docker to write files as your local user
 # see: https://github.com/pelias/docker#variable-docker_user
-echo "Setting DOCKER_USER variable as the local user in to .env file"
-sed -i '/DOCKER_USER/d' .env
-echo "DOCKER_USER=$(id -u)" >> .env
+# echo "Setting DOCKER_USER variable as the local user in to .env file"
+# sed -i '/DOCKER_USER/d' .env
+# echo "DOCKER_USER=$(id -u)" >> .env
